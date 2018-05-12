e0b9e688e820889df6a88d8de7bed118a8af751c
Pawel Pajak <pajakpawel@vp.pl>
2018-05-12 23:57:18 +0200

    Dodanie pliku potrzebnego w obsludze przycisku 'add to cart w liscie produktow

---
+++ PZ.Sklep/Utilities/LocalOnclickListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PZ.Sklep
+{
+    public class LocalOnclickListener : Java.Lang.Object, View.IOnClickListener
+    {
+        public void OnClick(View v)
+        {
+            HandleOnClick();
+        }
+        public System.Action HandleOnClick { get; set; }
+    }
+}
